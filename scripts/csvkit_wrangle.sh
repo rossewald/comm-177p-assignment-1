b2afd7e88cc0c39b250c967a59907b31a30a1727
Serdar Tumgoren <zstumgoren@gmail.com>
2020-02-08 09:42:24 -0800

    Feedbak

--- scripts/csvkit_wrangle.sh
+++ scripts/csvkit_wrangle.sh
@@ -1,9 +1,22 @@
+# FEEDBACK: Great job overall Ross! The code
+# is elegant and well-documented, and I appreciated
+# how you used the output flag for wget so that
+# subsequent runs of the script will work correctly.
+# However, note that there's one subtle but critical
+# bug in the program when you redirect the output
+# from csvgrep. It appears you used the "append" mode
+# of redirection (>>) as opposed to the simpler
+# redirect (>) which overwrites pre-existing content.
+# Because you're appending lines rather than over-writing,
+# the banks count will grow increasing large on subsequent runs
+# (unless you manually clear out failed_banks_ca.cs).
 
 # Get a CSV of all failed banks and save it to a local file called failed_banks.csv
 wget -O failed_banks.csv https://www.fdic.gov/bank/individual/failed/banklist.csv
 
 # Create a CSV of all failed banks in CA and save it to failed_banks_ca.csv
-csvgrep -m "CA" -c "3" failed_banks.csv >> failed_banks_ca.csv
+# NOTE: Use basic file redirection (>) rather than appending (>>)
+csvgrep -m "CA" -c "3" failed_banks.csv > failed_banks_ca.csv
 
 # Get the number of lines in this file.
 csv_size=$(cat failed_banks_ca.csv  | wc -l)
